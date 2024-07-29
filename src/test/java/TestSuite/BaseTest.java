a2cbf60daaf8779cf3fc5ad0ba77257331e8d9cb
Bella Luchko <xo4yshe4ka@yandex.ru>
2024-07-29 17:33:37 +0300

    Исправила подключение к браузерам

--- src/test/java/TestSuite/BaseTest.java
+++ src/test/java/TestSuite/BaseTest.java
@@ -1,41 +1,48 @@
 package TestSuite;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     protected WebDriver driver;
     private final static String BASE_URL = "https://qa-scooter.praktikum-services.ru/";
 
     @Before
-    public void setUp() {
-        driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\bin\\chromedriver.exe");
-        driver.manage().window().setSize(new Dimension(1280, 720));
+    public void setUpDriver() {
+        String driverType = System.getenv("WEB_DRIVER");
+        driver = getDriver(driverType == null ? "chrome" : driverType);
         driver.get(BASE_URL);
-        Cookie newCookie = new Cookie("Cartoshka", "true");
-        driver.manage().addCookie(newCookie);
-        Cookie newCookie2 = new Cookie("Cartoshka-legacy", "true");
-        driver.manage().addCookie(newCookie2);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        setCookie(new Cookie("Cartoshka", "true"));
+        setCookie(new Cookie("Cartoshka-legacy", "true"));
         driver.navigate().refresh();
     }
 
-    /*@Before
-    public void setUp() {
-        driver = new FirefoxDriver();
-        System.setProperty("webdriver.firefox.driver", "C:\\WebDriver\\bin\\geckodriver.exe");
-        driver.manage().window().setSize(new Dimension(1280, 720));
-        driver.get(BASE_URL);
-        Cookie newCookie = new Cookie("Cartoshka", "true");
-        driver.manage().addCookie(newCookie);
-        Cookie newCookie2 = new Cookie("Cartoshka-legacy", "true");
-        driver.manage().addCookie(newCookie2);
-        driver.navigate().refresh();
-    }*/
+    public WebDriver getDriver(String driverType) {
+        switch (driverType) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                return new ChromeDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return new FirefoxDriver();
+            default:
+                throw new IllegalArgumentException("Driver type is not supported");
+        }
+    }
+
+    public void setCookie(Cookie cookie) {
+        driver.manage().addCookie(cookie);
+    }
 
     @After
     public void tearDown() {
